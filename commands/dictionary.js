a3a07f8f5b2ad3c9296a6e89a5eb6b1867186674
Gaiwecoor <gaiwecoor@gmail.com>
2018-09-04 11:29:30 -0500

    !define initial commit

---
+++ commands/dictionary.js
@@ -0,0 +1,44 @@
+const Augur = require("../Augur"),
+  parseXML = require("xml2js").parseString,
+  u = require("../utils/utils"),
+  request = require("request");
+
+const Module = new Augur.Module()
+.addCommand({name: "define",
+  description: "Define a word",
+  syntax: "<word>",
+  alisases: ["dictionary"],
+  process: (msg, suffix) => {
+    if (!suffix) msg.reply("you need to give me a word to define!");
+    else {
+      let url = "https://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + encodeURIComponent(suffix) + "?key=" + Module.config.api.mw.dictionary;
+      request(url, (error, response, body) => {
+        if (!error && response && response.statusCode == 200) {
+          parseXML(body, {explicitArray: false}, (err, xml) => {
+            if (err) u.alertError(err, msg);
+            else {
+              let entry = xml.entry_list.entry;
+              let defs = entry.def.dt;
+              if (defs.length > 0) {
+                let description = `**${entry.ew}**\n\`${entry.hw}\` | \`${entry.pr}\`\n`;
+
+                defs.forEach((def, i) => description += `\n**${i + 1}**${(typeof def == "object" ? def._.replace("  ", ` ${def.d_link} `) : def)}`);
+
+                let embed = u.embed()
+                  .setTitle(entry.ew)
+                  .setDescription(description)
+                  .setColor([38, 120, 206])
+                  .setThumbnail("https://www.dictionaryapi.com/images/info/branding-guidelines/mw-logo-dark-background-100x100.png")
+                  .setFooter("Provided by Merriam-Webster's Collegiate® Dictionary");
+
+                msg.channel.send(embed);
+              }
+            }
+          });
+        } else u.alertError(error, msg);
+      });
+    }
+  }
+});
+
+module.exports = Module;
