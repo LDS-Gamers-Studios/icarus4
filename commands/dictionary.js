f7946a2d78e0bb484dfb4b3b2b9ae5690065d391
Gaiwecoor <gaiwecoor@gmail.com>
2018-09-04 12:31:15 -0500

    Multiple definitions

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -17,22 +17,26 @@ const Module = new Augur.Module()
           parseXML(body, (err, xml) => {
             if (err) u.alertError(err, msg);
             else {
-              let entry = xml.entry_list.entry[0];
-              let defs = entry.def[0].dt;
-              if (defs.length > 0) {
-                let description = `**${entry.ew}** *(${entry.fl})*\n\`${entry.hw}\` | \`${entry.pr}\`\n`;
+              let entries = xml.entry_list.entry.filter(e => e.ew && stringify(e.ew).toLowerCase() == suffix.toLowerCase());
+              let description = "";
+              let embed = u.embed()
+                .setColor([38, 120, 206])
+                .setThumbnail("https://www.dictionaryapi.com/images/info/branding-guidelines/mw-logo-dark-background-100x100.png")
+                .setFooter("Provided by Merriam-Webster's Collegiate® Dictionary");
 
-                defs.forEach((def, i) => description += `\n**${i + 1}**${(typeof def == "object" ? def._.replace("  ", ` ${def.d_link} `) : def)}`);
+              entries.forEach(entry => {
+                let defs = entry.def[0].dt;
+                if (defs.length > 0) {
+                  let info = [];
+                  ["hw", "pr"].forEach(e => { if (entry[e]) info.push(entry[e][0]); });
 
-                let embed = u.embed()
-                  .setTitle(entry.ew)
-                  .setDescription(description)
-                  .setColor([38, 120, 206])
-                  .setThumbnail("https://www.dictionaryapi.com/images/info/branding-guidelines/mw-logo-dark-background-100x100.png")
-                  .setFooter("Provided by Merriam-Webster's Collegiate® Dictionary");
+                  description += `\n\n**${stringify(entry.ew)}** *(${entry.fl})*\n` + info.map(i => `\`${stringify(i)}\``).join(" | ") + "\n";
+                  defs.forEach((def, i) => description += `\n**${i + 1}**${stringify(def)}`);
+                }
+              });
 
-                msg.channel.send(embed);
-              }
+              embed.setDescription(description.trim().slice(0, 2000));
+              msg.channel.send(embed);
             }
           });
         } else u.alertError(error, msg);
