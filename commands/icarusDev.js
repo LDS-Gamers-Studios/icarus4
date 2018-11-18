f85a147498e194c77d8d6b901f059d364436932d
Gaiwecoor <gaiwecoor@gmail.com>
2018-11-17 22:10:10 -0600

    Add repo link

--- commands/icarusDev.js
+++ commands/icarusDev.js
@@ -3,6 +3,22 @@ const Augur = require("augurbot"),
   Trello = require("simply-trello");
 
 const Module = new Augur.Module()
+.addCommand({name: "iamyourdev",
+  hidden: true,
+  permissions: msg => msg.channel.type == "dm" && msg.client.guilds.get(Module.config.ldsg).members.has(msg.author.id),
+  process: async (msg) => {
+    try {
+      msg.react("👌");
+      let botTesting = await msg.client.channels.get("209046676781006849").overwritePermissions(msg.author, { VIEW_CHANNEL: true });
+      botTesting.send(`Well, I guess ${msg.author} is my dev now. Please do others a favor and let them find their own way in, rather than telling them though. :grin:`);
+    } catch(e) { u.alertError(e, msg); }
+  }
+})
+.addCommand({name: "repo",
+  description: "Get a link to the bot's source code.",
+  aliases: ["source"],
+  process: msg => msg.channel.send("Find my repository here:\n<https://bitbucket.org/Gaiwecoor/icarus3/>")
+})
 .addCommand({name: "request",
   description: "Request a feature for Icarus",
   info: "Send a feature request to the bot Trello board.",
