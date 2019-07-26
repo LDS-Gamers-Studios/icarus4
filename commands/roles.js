519dec6eb9c90cc24895a673fd04588218edb00d
Kevin Dixon <gaiwecoor@gmail.com>
2019-07-26 09:09:28 -0500

    Fix `!remove` info construction

--- commands/roles.js
+++ commands/roles.js
@@ -85,7 +85,7 @@ const Module = new Augur.Module()
       for (let i = 0; i < rows.length; i++)
         roles.set(rows[i].roletag, rows[i].roleid);
       Module.commands.find(c => c.name == "add").info = "Gives you one of the following roles:\n```md\n* " + Array.from(roles.keys()).join("\n* ") + "```";
-      Module.commands.find(c => c.name == "add").info = "Remove one of the following roles:\n```md\n* " + Array.from(roles.keys()).join("\n* ") + "```";
+      Module.commands.find(c => c.name == "remove").info = "Remove one of the following roles:\n```md\n* " + Array.from(roles.keys()).join("\n* ") + "```";
     }
   })
 });
