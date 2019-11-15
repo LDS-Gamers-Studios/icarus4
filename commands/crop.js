06b1a736e889d1ac98fa0090d7735336f2e0aedb
Brian Herbst <bherbst@solutionreach.com>
2019-11-15 09:59:05 -0700

    addCrop: addressed pr issues

--- commands/crop.js
+++ commands/crop.js
@@ -7,12 +7,12 @@ const Module = new Augur.Module()
   syntax: "crop",
   aliases: ["crop"],
   process: (msg, suffix) => {
-    processed = false;
+    let processed = false;
     msg.channel.fetchMessages({ limit: 100 }).then(messages => {
       if (processed){
         return;
       }
-      msg.forEach(async function (value, key, map) {
+      messages.forEach(async function (value, key, map) {
       if (processed ||value.author.bot){
         return;
       }
@@ -26,43 +26,42 @@ const Module = new Augur.Module()
           const Jimp = require("jimp");
           const cropped = await Jimp.read(a.url);
 
-          ogw= cropped.bitmap.width
-          ogh= cropped.bitmap.height
+          let ogw= cropped.bitmap.width
+          let ogh= cropped.bitmap.height
 
           // width
-          max = ogw * 9/10;
-          min = ogw /10;
-          nw = Math.floor(Math.random() * (max - min) + min);
+          let max = ogw * 9/10;
+          let min = ogw /10;
+          let nw = Math.floor(Math.random() * (max - min) + min);
 
           // height
           max = ogh * 9/10;
           min = ogh /10;
-          nh = Math.floor(Math.random() * (max - min) + min);
+          let nh = Math.floor(Math.random() * (max - min) + min);
 
           //starting x
           max=ogw -nw;
           min = 1;
-          var startX = Math.floor(Math.random() * (max - min) + min);  
+          let startX = Math.floor(Math.random() * (max - min) + min);  
 
           //starting y
           max=ogh -nh;
           min = 1;
-          var startY = Math.floor(Math.random() * (max - min) + min);  
+          let startY = Math.floor(Math.random() * (max - min) + min);  
 
 
           cropped.crop(startX, startY, nw, nh)
           const canvas = new Jimp(nw, nh, 0x000000);
           canvas.blit(cropped,0,0);
           await msg.channel.send({files: [await canvas.getBufferAsync(Jimp.MIME_PNG)]});
-        } catch(e) { console.log(e) }
+        } catch(e) { u.alertError(e, msg) }
 
         });
       }
       })
     })
-    .catch(console.error);
-    message.delete();
-
+    .catch(e => u.alertError(e, msg));
+    u.clean(msg);
   }
 })
 
