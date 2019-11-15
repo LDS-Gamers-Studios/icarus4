da9a6fd88ea67040d0ae5060809a5cd15470f441
Brian Herbst <bherbst@solutionreach.com>
2019-11-15 10:16:06 -0700

    addCrop: changed a few loops

--- commands/crop.js
+++ commands/crop.js
@@ -12,53 +12,48 @@ const Module = new Augur.Module()
       if (processed){
         return;
       }
-      messages.forEach(async function (value, key, map) {
-      if (processed ||value.author.bot){
-        return;
-      }
-      if (value.attachments.size >0){
-        value.attachments.forEach(async function (a, k, m){
-        if (processed){
-          return;
+      for (const [key, value] of messages) {
+        if (processed ||value.author.bot){
+          break;
         }
-        processed =true;
-        try {
-          const Jimp = require("jimp");
-          const cropped = await Jimp.read(a.url);
+        if (value.attachments.size >0){
+          let a=value.attachments.first();
+          processed =true;
+          try {
+            const Jimp = require("jimp");
+            const cropped = await Jimp.read(a.url);
 
-          let ogw= cropped.bitmap.width
-          let ogh= cropped.bitmap.height
+            let ogw= cropped.bitmap.width
+            let ogh= cropped.bitmap.height
 
-          // width
-          let max = ogw * 9/10;
-          let min = ogw /10;
-          let nw = Math.floor(Math.random() * (max - min) + min);
+            // width
+            let max = ogw * 9/10;
+            let min = ogw /10;
+            let nw = Math.floor(Math.random() * (max - min) + min);
 
-          // height
-          max = ogh * 9/10;
-          min = ogh /10;
-          let nh = Math.floor(Math.random() * (max - min) + min);
+            // height
+            max = ogh * 9/10;
+            min = ogh /10;
+            let nh = Math.floor(Math.random() * (max - min) + min);
 
-          //starting x
-          max=ogw -nw;
-          min = 1;
-          let startX = Math.floor(Math.random() * (max - min) + min);  
+            //starting x
+            max=ogw -nw;
+            min = 1;
+            let startX = Math.floor(Math.random() * (max - min) + min);  
 
-          //starting y
-          max=ogh -nh;
-          min = 1;
-          let startY = Math.floor(Math.random() * (max - min) + min);  
+            //starting y
+            max=ogh -nh;
+            min = 1;
+            let startY = Math.floor(Math.random() * (max - min) + min);  
 
 
-          cropped.crop(startX, startY, nw, nh)
-          const canvas = new Jimp(nw, nh, 0x000000);
-          canvas.blit(cropped,0,0);
-          await msg.channel.send({files: [await canvas.getBufferAsync(Jimp.MIME_PNG)]});
-        } catch(e) { u.alertError(e, msg) }
-
-        });
+            cropped.crop(startX, startY, nw, nh)
+            const canvas = new Jimp(nw, nh, 0x000000);
+            canvas.blit(cropped,0,0);
+            await msg.channel.send({files: [await canvas.getBufferAsync(Jimp.MIME_PNG)]});
+          } catch(e) { u.alertError(e, msg) }
+        }
       }
-      })
     })
     .catch(e => u.alertError(e, msg));
     u.clean(msg);
