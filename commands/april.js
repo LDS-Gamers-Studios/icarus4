74c8bfa50be96206df16f235919f16f370a586c3
Kevin Dixon <gaiwecoor@gmail.com>
2020-03-06 12:07:46 -0600

    April test

---
+++ commands/april.js
@@ -0,0 +1,25 @@
+const Augur = require("augurbot"),
+  u = require("../utils/utils");
+
+const Module = new Augur.Module()
+.addEvent("message", (msg) => {
+  try {
+    let date = new Date();
+    if (msg.guild && ((msg.guild.id == "136569499859025920") || (d.getMonth() == 3 && d.getDate() == 1))) {
+      let regex = /(?:^|\.|\?|!|\n)\s*(?:I am|I'm) (.*?)(?:\.|!|\?|\n|$)/i;
+      let match = regex.match(msg.cleanContent);
+      if (match) {
+        let name = u.properCase(match[1]);
+        message.member.setNickname(name)
+        .then(member => {
+          message.channel.send(`Hi, ${member}. I'm Icarus.`);
+        })
+        .catch(() => {
+          message.channel.send(`Hi, ${name}. I'm Icarus.`);
+        });
+      }
+    }
+  } catch(e) { u.alertError(e, msg); }
+});
+
+module.exports = Module;
