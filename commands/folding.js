a39d54a8fa38c1f708772da2f75a1b5587dcdf5e
Kevin Dixon <gaiwecoor@gmail.com>
2020-03-19 10:40:49 -0500

    Keep trying!

--- commands/folding.js
+++ commands/folding.js
@@ -2,11 +2,10 @@ const Augur = require("augurbot");
 const request = require("request");
 const u = require("../utils/utils");
 
-const Module = new Augur.Module()
-.addCommand({name: "fah",
-  aliases: ["foldingathome", "folding", "folding@home"],
-  description: "LDSG Folding@home Stats",
-  process: (msg) => {
+const attempts = new Map();
+
+function getStats(channel) {
+  return new Promise((fulfill, reject) => {
     request("https://stats.foldingathome.org/api/team/238415", async function(error, response, body) {
       try {
         if (!error && response.statusCode == 200) {
@@ -25,12 +24,38 @@ const Module = new Augur.Module()
             embed.addField(contrib.name, `**Profile:** [[Link]](https://stats.foldingathome.org/donor/${contrib.name})\n**Work Units:** ${contrib.wus}\n**Credit:** ${contrib.credit}${(contrib.rank ? "\n**Rank:** " + contrib.rank : "")}`, true);
           }
 
-          msg.channel.send({embed});
+          if (attempts.has(channel.id)) {
+            clearInterval(attempts.get(channel.id));
+            attempts.delete(channel.id);
+          }
+
+          channel.send({embed});
+
+          fulfill(true);
         } else if (response.statusCode != 200) {
-          msg.channel.send("Sorry, I ran into an error fetching LDSG Folding at Home stats. Try again in a bit.").then(u.clean);
+          fulfill(false);
         }
       } catch(error) { u.alertError(error, msg); }
-    })
+    });
+  });
+}
+
+const Module = new Augur.Module()
+.addCommand({name: "fah",
+  aliases: ["foldingathome", "folding", "folding@home"],
+  description: "LDSG Folding@home Stats",
+  process: async (msg) => {
+    try {
+      let success = await getStats(msg.channel);
+      if (!success) {
+        msg.channel.send("I'm having trouble connecting to the Folding @ Home website. I'll keep trying!");
+        if (attempts.has(channel.id)) {
+          clearInterval(attempts.get(channel.id));
+          attempts.delete(channel.id);
+        }
+        attempts.set(msg.channel.id, setInterval(getStats, 60000, msg.channel));
+      }
+    } catch(e) { u.alertError(e, msg); }
   }
 });
 
