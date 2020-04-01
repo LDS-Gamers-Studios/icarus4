22716d2bb2dab5fffebdbeea0e4351349a07aea9
Kevin Dixon <gaiwecoor@gmail.com>
2020-03-31 19:48:44 -0500

    Only trim if you have to

--- commands/april.js
+++ commands/april.js
@@ -22,7 +22,7 @@ const Module = new Augur.Module()
       if (match) {
         cooldown.set(msg.author.id, date.getTime());
         let name = u.properCase(match[1]);
-        msg.member.setNickname(name.substr(0, (name.lastIndexOf(" ", 32) > 0 ? name.lastIndexOf(" ", 32) : name.length)))
+        msg.member.setNickname(name.length > 32 ? name.substr(0, name.lastIndexOf(" ", 32)) : name)
         .finally(() => {
           msg.channel.send(`Hi, ${name}. I'm Icarus.`);
         });
