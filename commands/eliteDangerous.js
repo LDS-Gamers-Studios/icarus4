795d11fec8f2e27617c78b09df93a9274f6e92c8
GuyInGrey <guyingreydaboss@gmail.com>
2020-07-07 13:35:42 -0600

    Fixed bunches of issues with the Stations list

--- commands/eliteDangerous.js
+++ commands/eliteDangerous.js
@@ -5,113 +5,111 @@ const Augur = require("augurbot"),
   elite = require("../utils/eliteDangerousAPI");
 
 const Module = new Augur.Module()
-.addCommand({name: "elite",
-  description: "Elite Dangerous information. Use `!elite help` for more.",
-  syntax: "See `!elite help`",
-  aliases: [],
-  process: async (msg, suffix) => {
-    try {
-      let [command, ...params] = suffix.split(" ");
-      command = command.toLowerCase();
-      let remainder = params.join(" ");
+  .addCommand({
+    name: "elite",
+    description: "Elite Dangerous information. Use `!elite help` for more.",
+    syntax: "See `!elite help`",
+    aliases: [],
+    process: async (msg, suffix) => {
+      try {
+        let [command, ...params] = suffix.split(" ");
+        command = command.toLowerCase();
+        let remainder = params.join(" ");
 
-      if (command === "status") {
-        let status = elite.getEliteStatus();
-        var message = "The Elite: Dangerous servers are " + (status.type === "success" ? "online." : "offline.");
-        msg.channel.send(message);
-        return;
-      //
-      // Help
-      //
-      } else if (command === "help") {
-        embed.addField("Get Elite's Server Status", "!elite status");
-        embed.addField("Get System Information", "!elite system **System Name**");
-        embed.addField("Get Stations in a System", "!elite stations **System Name**\n(Note: Will not show Fleet Carriers)");
-        embed.addField("Get Factions in a System", "!elite factions **System Name**");
-        embed.addField("Get Celestial Bodies in a System", "!elite bodies **System Name**");
+        if (command === "status") {
+          let status = elite.getEliteStatus();
+          var message = "The Elite: Dangerous servers are " + (status.type === "success" ? "online." : "offline.");
+          msg.channel.send(message);
+          return;
+          //
+          // Help
+          //
+        } else if (command === "help") {
+          embed.addField("Get Elite's Server Status", "!elite status");
+          embed.addField("Get System Information", "!elite system **System Name**");
+          embed.addField("Get Stations in a System", "!elite stations **System Name**\n(Note: Will not show Fleet Carriers)");
+          embed.addField("Get Factions in a System", "!elite factions **System Name**");
+          embed.addField("Get Celestial Bodies in a System", "!elite bodies **System Name**");
 
-        msg.channel.send({ embed });
-        return;
-      }
-
-      let starSystem = await elite.getSystemInfo(remainder);
-      if (!starSystem) {
-        msg.channel.send("I couldn't find a system with that name.").then(u.clean);
-        return;
-      }
-
-      let embed = u.embed()
-        .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
-        .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png")
-
-      if (command === "system") {
-        embed.setTitle(starSystem.name)
-          .setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/")
-          .addField("Permit Required?", starSystem.requirePermit ? "Yes" : "No", true);
-
-        if (starSystem.primaryStar) {
-          embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable ? "Yes" : "No", true);
+          msg.channel.send({ embed });
+          return;
         }
 
-        if (starSystem.information) {
-          embed.addField("Controlling Faction", starSystem.information.faction, true).
-          addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government, true);
-        } else {
-          embed.addField("Uninhabited System", "No faction information available.", true);
+        let starSystem = await elite.getSystemInfo(remainder);
+        if (!starSystem) {
+          msg.channel.send("I couldn't find a system with that name.").then(u.clean);
+          return;
         }
 
-        msg.channel.send({ embed });
-      //
-      // Stations
-      //
-      } else if (command === "stations") {
-        if (starSystem.stations.length <= 0) { msg.channel.send("I couldn't find any stations in that system."); return; }
+        let embed = u.embed()
+          .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
+          .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png")
 
-        embed.setTitle(starSystem.name)
-          .setURL(starSystem.stationsURL);
+        if (command === "system") {
+          embed.setTitle(starSystem.name)
+            .setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/")
+            .addField("Permit Required?", starSystem.requirePermit ? "Yes" : "No", true);
 
-        let stationList = {};
-
-        let i = 0;
-        for (let station of starSystem.stations) {
-          // Filtering out fleet carriers. There can be over 100 of them (spam) and their names are user-determined (not always clean).
-          if (station.type === "Fleet Carrier" || station.type === "Unknown") { continue; }
-          i++; if (i > 25) { continue; }
-
-          if (!stationList[station.type]) {
-            stationList[station.type] = [];
+          if (starSystem.primaryStar) {
+            embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable ? "Yes" : "No", true);
           }
-          stationList[station.type].push(station);
-        }
 
-        for (let stationType in stationList) {
-          embed.addField(stationType, "-----------------------------");
+          if (starSystem.information) {
+            embed.addField("Controlling Faction", starSystem.information.faction, true).
+              addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government, true);
+          } else {
+            embed.addField("Uninhabited System", "No faction information available.", true);
+          }
 
-          for (let station of stationList[stationType]) {
-            let stationURL = "https://www.edsm.net/en/system/stations/id/" + starSystem.id + "/name/" + starSystem.name + "/details/idS/" + station.id + "/";
-            let faction = "";
-            // Rounding to one decimal
-            let distance = Math.round(station.distanceToArrival * 10) / 10;
-            if (station.controllingFaction) {
-              faction = station.controllingFaction.name;
+          msg.channel.send({ embed });
+          //
+          // Stations
+          //
+        } else if (command === "stations") {
+          if (starSystem.stations.length <= 0) { msg.channel.send("I couldn't find any stations in that system."); return; }
+
+          embed.setTitle(starSystem.name)
+            .setURL(starSystem.stationsURL);
+
+          let stationList = new Map();
+
+          let i = 0;
+          for (let station of starSystem.stations) {
+            // Filtering out fleet carriers. There can be over 100 of them (spam) and their names are user-determined (not always clean).
+            if (station.type === "Fleet Carrier" || station.type === "Unknown") { continue; }
+            if (++i + stationList.size > 24) break;
+
+            if (!stationList.has(station.type)) stationList.set(station.type, []);
+            stationList.get(station.type).push(station);
+          }
+
+          for (const [stationType, stations] of stationList) {
+            embed.addField(stationType, "-----------------------------");
+
+            for (let station of stations) {
+              let stationURL = "https://www.edsm.net/en/system/stations/id/" + starSystem.id + "/name/" + starSystem.name + "/details/idS/" + station.id + "/";
+              let faction = "";
+              // Rounding to one decimal
+              let distance = Math.round(station.distanceToArrival * 10) / 10;
+              if (station.controllingFaction) {
+                faction = station.controllingFaction.name;
+              }
+
+              embed.addField(faction, "[" + station.name + "](" + encodeURI(stationURL) + ")\n" + distance + " ls", true);
             }
-
-            embed.addField(faction, "[" + station.name + "](" + encodeURI(stationURL) + ")\n" + distance + " ls", true);
           }
-        }
 
-        // Letting the user know there were more than 25
-        if (i > 25) {
-          embed.setFooter("Some stations were filtered out because the limit was exceeded.", "https://i.imgur.com/vYPj8iX.png");
-        }
+          // Letting the user know there were more than 25
+          if (i + stationList.size > 24) {
+            embed.setFooter("Some stations were filtered out because the limit was exceeded.", "https://i.imgur.com/vYPj8iX.png");
+          }
 
-        msg.channel.send({ embed });
-      //
-      // Factions
-      //
-      } else if (command === "factions")
-      {
-        if (starSystem.factions.length <= 0) { msg.channel.send("I couldn't find any factions in that system."); return; }
+          msg.channel.send({embed});
+          //
+          // Factions
+          //
+        } else if (command === "factions") {
+          if (starSystem.factions.length <= 0) { msg.channel.send("I couldn't find any factions in that system."); return; }
 
         embed.setTitle(starSystem.name)
           .setURL(starSystem.factionsURL);
@@ -124,29 +122,29 @@ const Module = new Augur.Module()
         }
 
         msg.channel.send({ embed });
-      //
-      // Bodies
-      //
-      } else if (command === "bodies") {
-        if (starSystem.bodies.length <= 0) { msg.channel.send("I couldn't find any bodies in that system."); return; }
+        //
+        // Bodies
+        //
+        } else if (command === "bodies") {
+          if (starSystem.bodies.length <= 0) { msg.channel.send("I couldn't find any bodies in that system."); return; }
 
-        embed.setTitle(starSystem.name)
-          .setURL(starSystem.bodiesURL);
+          embed.setTitle(starSystem.name)
+            .setURL(starSystem.bodiesURL);
 
-        for (let body of starSystem.bodies) {
-          //                                                                                 Yes, this double slash is intentional
-          let url = encodeURI("https://www.edsm.net/en/system/bodies/id/" + starSystem.id + "//details/idB/" + body.id + "/nameB/");
-          let scoopable = body.type === "Star" ? (body.isScoopable ? " (Scoopable)" : " (Not Scoopable)") : "";
-          let distance = Math.round(body.distanceToArrival * 10) / 10;
-          embed.addField(body.name, body.type + scoopable + "\n" + distance + " ls");
-        }
+          for (let body of starSystem.bodies) {
+            //                                                                                 Yes, this double slash is intentional
+            let url = encodeURI("https://www.edsm.net/en/system/bodies/id/" + starSystem.id + "//details/idB/" + body.id + "/nameB/");
+            let scoopable = body.type === "Star" ? (body.isScoopable ? " (Scoopable)" : " (Not Scoopable)") : "";
+            let distance = Math.round(body.distanceToArrival * 10) / 10;
+            embed.addField(body.name, body.type + scoopable + "\n" + distance + " ls");
+          }
 
-        msg.channel.send({ embed });
-      } else {
-        msg.channel.send("I didn't understand that command. See `!elite help`.").then(u.clean);
-      }
-    } catch(error) { u.alertError(error, msg); }
-  }
+          msg.channel.send({ embed });
+        } else {
+          msg.channel.send("I didn't understand that command. See `!elite help`.").then(u.clean);
+        };
+      } catch (error) { u.alertError(error, msg); }
+    }
 });
 
 module.exports = Module;
