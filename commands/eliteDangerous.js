976e3cd883f5c0139577d53fa172584a5a87062b
Kevin Dixon <gaiwecoor@gmail.com>
2020-07-06 21:08:19 -0500

    Burn the switches!

--- commands/eliteDangerous.js
+++ commands/eliteDangerous.js
@@ -12,58 +12,63 @@ const Module = new Augur.Module()
   process: async (msg, suffix) => {
     try {
       let [command, ...params] = suffix.split(" ");
+      command = command.toLowerCase();
       let remainder = params.join(" ");
-      let starSystem = await elite.getSystemInfo(remainder);
-      if (!starSystem) { msg.channel.send("I couldn't find a system with that name.").then(u.clean); return; }
 
       let embed = u.embed()
-      .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
-      .setTitle(starSystem.name)
-      .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png");
+        .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
+        .setTitle(starSystem.name)
+        .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png");
 
-      switch (command) {
-        // Returns help for subcommands.
-        // Will be added once the rest is done.
-        case "help":
-          msg.channel.send("Not *yet* implemented. I might work faster if you give me a <:buttermelon:305039588014161921>");
-          break;
-        case "system":
-          embed.setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/")
-            .addField("Permit Required?", starSystem.requirePermit ? "Yes" : "No", true);
+      if (command == "help") {
+        msg.channel.send("Not *yet* implemented. I might work faster if you give me a <:buttermelon:305039588014161921>");
+      } else if (command == "system") {
+        let starSystem = await elite.getSystemInfo(remainder);
+        if (!starSystem) {
+          msg.channel.send("I couldn't find a system with that name.").then(u.clean);
+          return;
+        }
 
-          if (starSystem.primaryStar) {
-            embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable ? "Yes" : "No", true);
+        embed.setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/")
+        .addField("Permit Required?", starSystem.requirePermit ? "Yes" : "No", true);
+
+        if (starSystem.primaryStar) {
+          embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable ? "Yes" : "No", true);
+        }
+
+        if (starSystem.information) {
+          embed.addField("Controlling Faction", starSystem.information.faction, true).
+          addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government, true);
+        } else {
+          embed.addField("Uninhabited System", "No faction information available.", true);
+        }
+
+        msg.channel.send({embed});
+      } else if (command == "stations") {
+        let starSystem = await elite.getSystemInfo(remainder);
+        if (!starSystem) {
+          msg.channel.send("I couldn't find a system with that name.").then(u.clean);
+          return;
+        }
+
+        if (!starSystem) { msg.channel.send("I couldn't find a system with that name."); return; }
+        if (starSystem.stations.length <= 0) { msg.channel.send("I couldn't find any stations in that system."); return; }
+
+        embed.setURL(starSystem.stationsURL);
+
+        for (let station of starSystem.stations.filter((e, i) => i < 25)) {
+          let stationURL = "https://www.edsm.net/en/system/stations/id/" + starSystem.id + "/name/" + starSystem.name + "/details/idS/" + station.id + "/";
+          let faction = "";
+          if (station.controllingFaction) {
+            faction = " - " + station.controllingFaction.name;
           }
 
-          if (starSystem.information) {
-            embed.addField("Controlling Faction", starSystem.information.faction, true).
-              addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government, true);
-          } else {
-            embed.addField("Uninhabited System", "No faction information available.", true);
-          }
+          embed.addField(station.name, "[**" + station.type + "** - " + station.distanceToArrival + " LS" + faction + "](" + stationURL + ")");
+        }
 
-          msg.channel.send({embed});
-          break;
-        case "stations":
-          if (!starSystem) { msg.channel.send("I couldn't find a system with that name."); return; }
-          if (starSystem.stations.length <= 0) { msg.channel.send("I couldn't find any stations in that system."); return; }
-
-          embed.setURL(starSystem.stationsURL);
-
-          for (let station of starSystem.stations.filter((e, i) => i < 25)) {
-            let stationURL = "https://www.edsm.net/en/system/stations/id/" + starSystem.id + "/name/" + starSystem.name + "/details/idS/" + station.id + "/";
-            let faction = "";
-            if (station.controllingFaction) {
-              faction = " - " + station.controllingFaction.name;
-            }
-
-            embed.addField(station.name, "[**" + station.type + "** - " + station.distanceToArrival + " LS" + faction + "](" + stationURL + ")");
-          }
-
-          msg.channel.send({ embed });
-          break;
-        default:
-          msg.channel.send("I didn't understand that command. See `!elite help`.").then(u.clean);
+        msg.channel.send({ embed });
+      } else {
+        msg.channel.send("I didn't understand that command. See `!elite help`.").then(u.clean);
       }
     } catch(error) { u.alertError(error, msg); }
   }
