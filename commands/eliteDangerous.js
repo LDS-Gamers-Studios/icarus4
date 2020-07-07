bddec46a585ffdc914c97fdde98c39f7b4ea94a9
GuyInGrey <guyingreydaboss@gmail.com>
2020-07-06 19:28:43 -0600

    Added API Wrapper, Added More Sub-Commands

--- commands/eliteDangerous.js
+++ commands/eliteDangerous.js
@@ -1,13 +1,8 @@
-const Augur = require("augurbot"),
-  u = require("../utils/utils");
-const request = require("request-promise-native");
+// Message GuyInGrey if this command has issues!
 
-async function getFromAPI(params) {
-  try {
-    let url = "https://www.edsm.net/api-v1/" + params;
-    return JSON.parse(await request(url));
-  } catch(error) { u.alertError(error, "Elite Dangerous API Error"); }
-}
+const Augur = require("augurbot"),
+  u = require("../utils/utils"),
+  elite = require("../utils/eliteDangerousAPI");
 
 const Module = new Augur.Module()
 .addCommand({name: "elite",
@@ -22,33 +17,61 @@ const Module = new Augur.Module()
       {
         // Returns help for subcommands.
         // Will be added once the rest is done.
-        case "help":
+        case "help": {
           msg.channel.send("Not *yet* implemented. I might work faster if you give me a <:buttermelon:305039588014161921>");
           break;
-        case "system":
-          let starSystem = await getFromAPI("system?showPrimaryStar=1&showInformation=1&showPermit=1&showId=1&systemName=" + remainder);
-          if (starSystem && starSystem.name) {
-            let embed = u.embed();
-            embed.setThumbnail("https://i.imgur.com/Ud8MOzY.png");
-            embed.setTitle(starSystem.name);
-            if (starSystem.information) {
-              embed.addField("Controlling Faction", starSystem.information.faction);
-              embed.addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government);
-            } else {
-              embed.addField("Uninhabited System", "No faction information available.");
-            }
-            embed.addField("Permit Required", starSystem.requirePermit);
-            if (starSystem.primaryStar) {
-              embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable);
-            }
-            embed.setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/" + starSystem.name.replace(" ", "+"));
-            msg.channel.send({embed});
+        }
+        case "system": {
+          let starSystem = await elite.getSystemInfo(remainder);
+          if (!starSystem) { msg.channel.send("I couldn't find a system with that name."); return; }
+
+          let embed = u.embed()
+            .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
+            .setTitle(starSystem.name)
+            .setURL("https://www.edsm.net/en/system/id/" + starSystem.id + "/name/")
+            .addField("Permit Required?", starSystem.requirePermit ? "Yes" : "No", true)
+            .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png");
+
+          if (starSystem.primaryStar) {
+            embed.addField("Star Scoopable", starSystem.primaryStar.isScoopable ? "Yes" : "No", true);
+          }
+
+          if (starSystem.information) {
+            embed.addField("Controlling Faction", starSystem.information.faction, true).
+              addField("Government Type", starSystem.information.allegiance + " - " + starSystem.information.government, true);
           } else {
-            msg.channel.send("I couldn't find a system with that name.");
-          };
+            embed.addField("Uninhabited System", "No faction information available.", true);
+          }
+
+          msg.channel.send({embed});
           break;
+        }
+        case "stations": {
+          let starSystem = await elite.getSystemInfo(remainder);
+          if (!starSystem) { msg.channel.send("I couldn't find a system with that name."); return; }
+          if (starSystem.stations.length <= 0) { msg.channel.send("I couldn't find any stations in that system."); return; }
+
+          let embed = u.embed()
+            .setThumbnail("https://i.imgur.com/Ud8MOzY.png")
+            .setTitle(starSystem.name)
+            .setURL(starSystem.stationsURL)
+            .setAuthor("EDSM", "https://i.imgur.com/4NsBfKl.png");
+
+          for (let station of starSystem.stations) {
+            let stationURL = "https://www.edsm.net/en/system/stations/id/" + starSystem.id + "/name/" + starSystem.name + "/details/idS/" + station.id + "/";
+            let faction = "";
+            if (station.controllingFaction) {
+              faction = " - " + station.controllingFaction.name;
+            }
+
+            embed.addField(station.name, "[**" + station.type + "** - " + station.distanceToArrival + " LS" + faction + "](" + stationURL + ")");
+          }
+
+          msg.channel.send({ embed });
+          break;
+        }
         default:
-          msg.channel.send("I didn't understand that command.").then(u.clean);
+          msg.channel.send("I didn't understand that command. See `!elite help`.").then(u.clean);
       }
     } catch(error) { u.alertError(error, "Elite Dangerous API Error"); }
   }
---
+++ utils/eliteDangerousAPI.js
@@ -0,0 +1,57 @@
+// Message GuyInGrey if this has issues!
+
+const request = require("request-promise-native");
+
+// Note: The EDSM rate limit is 360/hour. More than enough for this server's usage.
+let apiUrl = "https://www.edsm.net/";
+
+function getSystemInfo(systemName) {
+  return new Promise(async (fulfill, reject) => {
+    try {
+      let starSystem = await getObjectFromAPI("api-v1/system?showPrimaryStar=1&showInformation=1&showPermit=1&showId=1&systemName=" + systemName);
+      if (Array.isArray(starSystem)) {
+        fulfill(null);
+      }
+      if (starSystem.information === {}) {
+        starSystem.information = null;
+      }
+
+      let bodiesResponse = await getObjectFromAPI("api-system-v1/bodies?systemName=" + systemName);
+      starSystem.bodies = bodiesResponse.bodies;
+      starSystem.bodiesURL = bodiesResponse.url;
+
+      let stationsResponse = await getObjectFromAPI("api-system-v1/stations?systemName=" + systemName);
+      starSystem.stations = stationsResponse.stations;
+      starSystem.stationsURL = stationsResponse.url;
+
+      let factionsResponse = await getObjectFromAPI("api-system-v1/factions?systemName=" + systemName);
+      starSystem.factions = factionsResponse.factions;
+      starSystem.factionsURL = factionsResponse.url;
+
+      fulfill(starSystem);
+    } catch (error) { reject(error); }
+  });
+}
+
+function getEliteStatus() {
+  return new Promise(async (fulfill, reject) => {
+    try {
+      let status = await getObjectFromAPI("api-status-v1/elite-server");
+      fulfill(status);
+    } catch (error) { reject(error); }
+  });
+}
+
+function getObjectFromAPI(params) {
+  return new Promise(async (fulfill, reject) => {
+    try {
+      let url = apiUrl + params;
+      fulfill(JSON.parse(await request(url)));
+    } catch (error) { reject(error); }
+  });
+}
+
+module.exports = {
+  getSystemInfo,
+  getEliteStatus,
+};
