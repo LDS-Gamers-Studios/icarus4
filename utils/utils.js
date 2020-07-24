92cd507cd2121d799c072bc49a35f0424a6c64a4
Kevin Dixon <gaiwecoor@gmail.com>
2020-07-24 12:45:08 +0000

    Serves me right for using web interface

--- utils/utils.js
+++ utils/utils.js
@@ -58,7 +58,7 @@ const Utils = {
     }, t, msg);
     return Promise.resolve(msg);
   },
-  embed: (data) => new Discord.RichEmbed(data).setColor(config.color).setFooter("This post,
+  embed: (data) => new Discord.RichEmbed(data).setColor(config.color),
   errorLog: errorLog,
   escapeText: (txt) => txt.replace(/(\*|_|`|~|\\|\|)/g, '\\$1'),
   getUser: function(msg, user, strict = false) {
