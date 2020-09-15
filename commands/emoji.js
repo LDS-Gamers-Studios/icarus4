9c35dd1fd427bee1dfbe95819206e8b06ac23c33
Kevin Dixon <gaiwecoor@gmail.com>
2020-09-15 11:53:25 -0500

    Missing period?

--- commands/emoji.js
+++ commands/emoji.js
@@ -13,7 +13,7 @@ const Module = new Augur.Module()
     let test = /<(a?):(\w+):(\d+)>/i;
     let id = test.exec(suffix);
     let ext = (id[1] ? ".gif" : ".png");
-    if (id) msg.channel.send({files: [{name: id[2] + ext, attachment: `https://cdn.discordapp.com/emojis/${id[3]}${(id[1] ? "gif" : "png")}`}]});
+    if (id) msg.channel.send({files: [{name: id[2] + ext, attachment: `https://cdn.discordapp.com/emojis/${id[3]}.${(id[1] ? "gif" : "png")}`}]});
   },
   permissions: (msg) => (msg.guild && (msg.channel.permissionsFor(msg.author).has("ATTACH_FILES") || msg.channel.permissionsFor(msg.author).has("USE_EXTERNAL_EMOJIS") || msg.channel.permissionsFor(msg.author).has("EMBED_LINKS")))
 });
