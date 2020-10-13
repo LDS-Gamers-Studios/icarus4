6703b41a93a1a989df5d41adb7f8f61fa4701806
Kevin Dixon <gaiwecoor@gmail.com>
2020-10-13 09:50:54 -0500

    Don't do regex if you're not going to care

--- commands/buttermelon.js
+++ commands/buttermelon.js
@@ -3,26 +3,26 @@ const Augur = require("augurbot"),
   buttermelon = require("../data/buttermelon.json");
 
 function buttermelonEdit(msg) {
+  if ((msg.channel.id == "203518149809799168") && (msg.cleanContent.toLowerCase() == "test"))
+    msg.channel.send((Math.random() < 0.8 ? "`pass`" : "`fail`"));
+
   let exclude = ['121033996439257092', '164784857296273408'];
-  if (!msg.author.bot && !exclude.includes(msg.channel.id)) {
+  let roll = Math.random();
+  if (roll < .3 && !msg.author.bot && !exclude.includes(msg.channel.id)) {
     //let banana = /[bß8ƥɓϐβбБВЬЪвᴮᴯḃḅḇÞ][a@∆æàáâãäåāăȁȃȧɑαдӑӓᴀᴬᵃᵅᶏᶐḁạảấầẩẫậắằẳẵặ4Λ]+([nⁿńňŋƞǹñϰпНhийӣӥѝνṅṇṉṋ]+[a@∆æàáâãäåāăȁȃȧɑαдӑӓᴀᴬᵃᵅᶏᶐḁạảấầẩẫậắằẳẵặ4Λ]+){2}/ig;
-    let roll = Math.random();
     if (/bananas/.test(msg.content.toLowerCase())) {
       if (roll < .1)
         msg.channel.send({files: ["https://cdn.discordapp.com/attachments/154625360514777088/239045323522179073/buttermelons.jpg"]}).catch(u.errorHandler);
-      else if (roll < .3)
+      else
         msg.channel.send("*buttermelons").catch(u.errorHandler);
     } else if (/banana/.test(msg.content.toLowerCase().replace(/(\*|_)/ig, ""))) {
       if (roll < .06)
         msg.channel.send({files: ["https://cdn.discordapp.com/attachments/136577505418018826/238764601951387648/buttermelon.jpg"]}).catch(u.errorHandler);
       else if (roll < .1)
         msg.channel.send({files: ["https://cdn.discordapp.com/attachments/96335850576556032/374995339997872128/YigaButtermelon_web.png"]}).catch(u.errorHandler);
-      else if (roll < .3)
+      else
         msg.channel.send("*buttermelon").catch(u.errorHandler);
     }
-
-    if ((msg.channel.id == "203518149809799168") && (msg.cleanContent.toLowerCase() == "test"))
-      msg.channel.send((Math.random() < 0.8 ? "`pass`" : "`fail`"));
   }
 }
 
