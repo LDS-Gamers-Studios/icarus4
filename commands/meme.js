90fdccce139ce1700a49d3e053a8f28c5aae80a9
Kevin Dixon <gaiwecoor@gmail.com>
2020-10-14 21:14:12 -0500

    Permissions check, remove unnecessary `.toSting()`

--- commands/meme.js
+++ commands/meme.js
@@ -5,6 +5,7 @@ const Module = new Augur.Module()
     .addCommand({
         name: "meme",
         description: "Creates a meme, put an image URL for you background and then put the text you want along the bottom. Or put the image source afterwards. Who am I to judge?",
+        permissions: (msg) => msg.member && msg.channel.permissionsFor(msg.member).has(["ATTACH_FILES", "EMBED_LINKS"]),
         process: (msg, suffix) => {
             //general globals from bot this was imported from
             const args = suffix.trim().split(/ +/);
@@ -15,23 +16,23 @@ const Module = new Augur.Module()
                 const url = new RegExp(urlRegex, 'i');
                 return str.length < 2083 && url.test(str);
             }
-            
+
             let src = "https://i.imgflip.com/qbm81.jpg";
             let bottomText = "_";
             let topText = "_";
             args.forEach(element => {
-                if (isURL(element.toString())) {
-                    src = element.toString();
+                if (isURL(element)) {
+                    src = element;
                 }
-                else if (element.toString().toLowerCase() != "-t") {
-                    bottomText = bottomText.concat(element.toString() + " ");
+                else if (element.toLowerCase() != "-t") {
+                    bottomText = bottomText.concat(element + " ");
                 }
                 else {
                     topText = bottomText;
                     bottomText = "_";
                 }
             });
-            bottomText = encodeURIComponent(bottomText.trim());       
+            bottomText = encodeURIComponent(bottomText.trim());
             topText = encodeURIComponent(topText.trim());
             src = encodeURIComponent(src.trim());
             let meme = `https://api.memegen.link/images/custom/${topText}/${bottomText}.png?background=${src}`;
