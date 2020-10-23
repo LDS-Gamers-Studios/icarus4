95a1801ab6db3a56510abd90829f01af61f15e87
GuyInGrey <guyingreydaboss@gmail.com>
2020-10-23 08:11:07 -0600

    Minecraft Skins initial commit
    * Minecraft API
    
    * Cleaned things up with Minecraft.
    
    * Require `utils/utils.js`
    
    * Find the right IGN
    
    * Reformat string to correct D.js 12 breaks (don't want to display the Discord Id)
    
    * More meaningful error messaging
    
    * Expose functions to be required
    
    Co-authored-by: Gaiwecoor <gaiwecoor@gmail.com>

---
+++ commands/minecraft.js
@@ -0,0 +1,45 @@
+const Augur = require("augurbot"),
+  request = require("request-promise-native"),
+  u = require("../utils/utils),
+  minecraft = require("../utils/minecraftAPI");
+
+const Module = new Augur.Module()
+  .addCommand({
+    name: "minecraftskin",
+    description: "Gets the Minecraft skin of a user.",
+    syntax: "[@user]",
+    category: "Minecraft",
+    process: async (msg, suffix) => {
+      let user = false,
+        name = false;
+
+      if (u.userMentions(msg).size > 0) {
+        user = u.userMentions(msg).first();
+      } else if (!suffix) {
+        user = msg.author;
+      }
+
+      if (user) {
+        let ign = await Module.db.ign.find(user.id, 'minecraft');
+        if (ign) name = encodeURIComponent(ign.ign);
+        else {
+          msg.channel.send(`${user} has not set a Minecraft name with \`!addign minecraft\`.`).then(u.clean);
+          return;
+        }
+      } else name = encodeURIComponent(suffix);
+
+      try {
+        let uuid = await minecraft.getPlayerUUID(name);
+        if (!uuid) {
+          msg.channel.send("I couldn't find a Minecraft account with the username `" + name + "`.").then(u.clean);
+          return;
+        }
+
+        // The "body" part of this has other options for other skin views, that can be implemented later.
+        let skinUrl = "https://crafatar.com/renders/body/" + uuid;
+        msg.channel.send({ files: [skinUrl] });
+      } catch (e) { u.errorHandler(e, msg); }
+    }
+});
+
+module.exports = Module;
