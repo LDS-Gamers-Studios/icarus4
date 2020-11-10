0699733fac197f8867266b1827349cd5828404ba
Kevin Dixon <gaiwecoor@gmail.com>
2020-11-10 11:18:28 -0600

    Calculate progress based on actual goal

--- commands/streaming.js
+++ commands/streaming.js
@@ -352,7 +352,7 @@ const Module = new Augur.Module()
       embed.setTitle("LDSG Extra Life Team")
       .setThumbnail("https://assets.donordrive.com/extralife/images/fbLogo.jpg?v=202009241356")
       .setURL("https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=412575#donate")
-      .setDescription(`LDSG is raising money for Extra Life! We are currently at $${total} of our team's $${team.fundraisingGoal} goal for 2020. That's ${Math.round(100 * total / 2500)}% there!\n\nYou can help by donating to one of the Extra Life Team below.`);
+      .setDescription(`LDSG is raising money for Extra Life! We are currently at $${total} of our team's $${team.fundraisingGoal} goal for 2020. That's ${Math.round(100 * total / team.fundraisingGoal)}% there!\n\nYou can help by donating to one of the Extra Life Team below.`);
       msg.channel.send({embed});
     } catch(error) { u.errorHandler(error, msg); }
   }
