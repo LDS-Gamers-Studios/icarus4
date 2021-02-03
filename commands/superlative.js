72e8abfb9a4a1829d28e94e2f03ac57bcabca334
Kevin Dixon <gaiwecoor@gmail.com>
2021-02-03 09:54:09 -0600

    Maybe fetch the message first?

--- commands/superlative.js
+++ commands/superlative.js
@@ -76,12 +76,12 @@ const starBoards = new u.Collection()
 
 async function checkStarBoard(reaction, user) {
   try {
+    if (reaction.message?.partial !== false)
+      reaction.message = await reaction.message.fetch().catch(u.noop);
     if (reaction.partial)
       reaction = await reaction.fetch().catch(u.noop);
-    if (reaction?.message.partial)
-      reaction.message = await reaction.message.fetch().catch(u.noop);
 
-    const msg = reaction.message;
+    const msg = reaction?.message;
     if (!user.bot && msg?.guild?.id == Module.config.ldsg && (msg?.createdTimestamp > (Date.now() - 7 * 24 * 60 * 60000))) {
       // Only respond to recent messages from LDSG
       let react = reaction.emoji.name;
