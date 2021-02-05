a734590533b04471ed95b6574a8310386aa3079f
Kevin Dixon <gaiwecoor@gmail.com>
2021-02-05 13:00:04 -0600

    `!slowmode off`

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -12,22 +12,29 @@ const Module = new Augur.Module()
   permissions: (msg) => (msg.member && (msg.member.roles.cache.has(Module.config.roles.mod) || msg.member.roles.cache.has(Module.config.roles.management))),
   process: async (msg, suffix) => {
     try {
-      let [duration, cooldown] = suffix.split(" ").map(n => parseInt(n, 10));
-      if (duration) {
-        let prev = molasses.get(msg.channel.id);
-        if (prev) clearTimeout(prev.timeout);
-        let limit = prev ? prev.limit : msg.channel.rateLimitPerUser;
-        await msg.channel.edit({rateLimitPerUser: cooldown || 15});
-        msg.react("⏲️").catch(u.noop);
-        msg.guild.channels.cache.get("506575671242260490").send(`⏲️ ${msg.member.displayName} has set a ${cooldown || 15}s slow mode for ${duration} minutes in ${msg.channel}.`).catch(u.noop);
-        molasses.set(msg.channel.id, {
-          timeout: setTimeout((channel, rateLimitPerUser) => {
-            channel.edit({rateLimitPerUser}).catch(error => u.errorHandler(error, "Reset rate limit after slowmode"));
-            molasses.delete(channel.id);
-          }, duration * 60000, msg.channel, limit),
-          limit
-        });
-      } else msg.reply("you need to tell me how many minutes to slow things!").then(u.clean);
+      if (suffix.toLowerCase() == "off" || suffix == "0") {
+        msg.channel.edit({rateLimitPerUser: 0}).catch(e => u.errorHandler(e, msg));
+
+        if (molasses.has(msg.channel.id)) clearTimeout(molasses.get(msg.channel.id));
+        molasses.delete(msg.channel.id);
+      } else {
+        let [duration, cooldown] = suffix.split(" ").map(n => parseInt(n, 10));
+        if (duration) {
+          let prev = molasses.get(msg.channel.id);
+          if (prev) clearTimeout(prev.timeout);
+          let limit = prev ? prev.limit : msg.channel.rateLimitPerUser;
+          await msg.channel.edit({rateLimitPerUser: cooldown || 15});
+          msg.react("⏲️").catch(u.noop);
+          msg.guild.channels.cache.get("506575671242260490").send(`⏲️ ${msg.member.displayName} has set a ${cooldown || 15}s slow mode for ${duration} minutes in ${msg.channel}.`).catch(u.noop);
+          molasses.set(msg.channel.id, {
+            timeout: setTimeout((channel, rateLimitPerUser) => {
+              channel.edit({rateLimitPerUser}).catch(error => u.errorHandler(error, "Reset rate limit after slowmode"));
+              molasses.delete(channel.id);
+            }, duration * 60000, msg.channel, limit),
+            limit
+          });
+        } else msg.reply("you need to tell me how many minutes to slow things!").then(u.clean);
+      }
     } catch(error) { u.errorHandler(error, msg); }
   }
 });
