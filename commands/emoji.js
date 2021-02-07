8440b69c69ca23c06500e66477574775e51d1491
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2021-02-07 15:41:33 -0500

    Accept discord emojis
    Will require the installation of emojiUnicode, but I should be able to put that in the package.json file

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,5 +1,7 @@
 const Augur = require("augurbot"),
-  u = require("../utils/utils");
+  u = require("../utils/utils"),
+  Jimp = require('jimp'),
+  emojiUnicode = require('emoji-unicode');
 
 const Module = new Augur.Module()
 .addCommand({name: "emoji",
@@ -14,6 +16,13 @@ const Module = new Augur.Module()
     let id = test.exec(suffix);
     let ext = (id[1] ? ".gif" : ".png");
     if (id) msg.channel.send({files: [{name: id[2] + ext, attachment: `https://cdn.discordapp.com/emojis/${id[3]}.${(id[1] ? "gif" : "png")}`}]});
+    else if (emojiUnicode(suffix)){
+      let image = await Jimp.read(`https://twemoji.maxcdn.com/v/latest/72x72/${emojiUnicode(suffix)}.png`)
+      image.resize(200,200);
+      return message.channel.send({files: [await image.getBufferAsync(Jimp.MIME_PNG)]})
+    }
+        
+        
   },
   permissions: (msg) => (msg.guild && (msg.channel.permissionsFor(msg.author).has("ATTACH_FILES") || msg.channel.permissionsFor(msg.author).has("USE_EXTERNAL_EMOJIS") || msg.channel.permissionsFor(msg.author).has("EMBED_LINKS")))
 });
