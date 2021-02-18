57462a7c7ba80f28bdf20b2c340d6debc48bd29d
chevyboys <volcanicchevy@gmail.com>
2021-02-17 18:41:31 -0700

    Update commands/meme.js
    Co-authored-by: Gaiwecoor <gaiwecoor@gmail.com>

--- commands/meme.js
+++ commands/meme.js
@@ -55,40 +55,5 @@ const Module = new Augur.Module()
             u.log(msg);
             u.clean(msg, 2000);
         },
-    }).addCommand({
-        name: "behold",
-        category: "Meme",
-        description: "creates a doofenshritz behold meme, with the arguments becoming your bottom text. Small chance of fire.",
-        //permissions: (msg) => msg.channel.id == "121755900313731074" && msg.channel.permissionsFor(msg.member).has(["ATTACH_FILES", "EMBED_LINKS"]),
-        process: (msg) => {
-            function randomNumber(min, max) {
-                let high = Math.max(max, min);
-                let low = Math.min(max, min);
-                return Math.floor(Math.random() * (high - low + 1)) + low;
-            }
-            u.log(msg);
-            //Roll a D20. on a 20 get a squirell. On a 1, elmo burns. anything else you get heinz
-            function backgroundImage(override) {
-                switch (override || randomNumber(1, 20)) {
-                    case 1: return `https://static1.srcdn.com/wordpress/wp-content/uploads/2020/05/Elmo-Flames-Meme.jpg?q=50&fit=crop&w=960&h=500`;
-                    case 20: return `https://i.imgflip.com/18kirh.jpg`;
-                    default: return `https://i.imgflip.com/zs91u.jpg`;
-                }
-            }
-            let { command, suffix } = u.parse(msg, true);
-            let content = `${Module.config.prefix}${command} ${backgroundImage()} Behold! -t ${suffix}`;
-
-            let fakeMsg = {
-                channel: msg.channel,
-                guild: msg.guild,
-                member: msg.member,
-                author: msg.author,
-                attachments: msg.attachments,
-                content: content,
-                client: msg.client,
-                cleanContent: content
-            };
-            Module.client.commands.execute("meme", fakeMsg, content);
-        }
     });
 module.exports = Module;
